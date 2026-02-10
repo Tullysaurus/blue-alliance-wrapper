62e1ac70bdfe610e68530bc3066b67b49156e413
Tullysaurus <tullypro69@gmail.com>
2026-02-10 11:09:19 -0600

    add console log

--- src/app/(api)/api/v1/team/[teamId]/update/route.ts
+++ src/app/(api)/api/v1/team/[teamId]/update/route.ts
@@ -2,7 +2,9 @@ import { NextResponse } from "next/server";
 
 
 export async function GET(request: Request){
-    
+    const data = await request.json();
+
+    console.log(data);
     
     return NextResponse.json({ status: "success" });
 }
